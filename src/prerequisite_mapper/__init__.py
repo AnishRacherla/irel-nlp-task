c76c7e5a3f606d1a624711dfcb4255d6b8662a4b
Anish Racherla <racherlaanish@gmail.com>
2026-03-12 11:15:51 +0530

    Fix: remove missing mapper import from prerequisite_mapper __init__

--- src/prerequisite_mapper/__init__.py
+++ src/prerequisite_mapper/__init__.py
@@ -1,6 +1,5 @@
 """Prerequisite mapping module"""
 
-from .mapper import PrerequisiteMapper
 from .hybrid_mapper import HybridPrerequisiteMapper
 
-__all__ = ['PrerequisiteMapper', 'HybridPrerequisiteMapper']
+__all__ = ['HybridPrerequisiteMapper']
