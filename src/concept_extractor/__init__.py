52a4824b67d90c5233efe309192ba12223b086cb
Anish Racherla <racherlaanish@gmail.com>
2026-03-12 11:14:22 +0530

    Fix: remove missing extractor import from concept_extractor __init__

--- src/concept_extractor/__init__.py
+++ src/concept_extractor/__init__.py
@@ -1,6 +1,5 @@
 """Concept extraction module"""
 
-from .extractor import ConceptExtractor
 from .hybrid_extractor import HybridConceptExtractor
 
-__all__ = ['ConceptExtractor', 'HybridConceptExtractor']
+__all__ = ['HybridConceptExtractor']
